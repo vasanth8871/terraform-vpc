7163cf6dbe53fcd81f55addbc6914aa4fca9e598
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2023-06-18 11:37:43 +0530

    Update 06-subnet.tf

--- 06-subnet.tf
+++ 06-subnet.tf
@@ -18,46 +18,6 @@ resource "aws_subnet" "public1" {
 }
 
 
-###################################################################
-# Creating Public Subnet2
-###################################################################
-
-resource "aws_subnet" "public2" {
-    
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr,var.vpc_subnets, 1)
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.az.names[1]
-  tags = {
-    Name = "${var.project}-public2"
-    Project = var.project
-  }
-    
-  lifecycle {
-    create_before_destroy = false
-  }
-}
-
-
-###################################################################
-# Creating Public Subnet3
-###################################################################
-
-resource "aws_subnet" "public3" {
-    
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr,var.vpc_subnets,2)
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.az.names[2]
-  tags = {
-    Name = "${var.project}-public3"
-    Project = var.project
-  }
-  lifecycle {
-    create_before_destroy = false
-  }
-    
-}
 
 
 ###################################################################
@@ -79,40 +39,3 @@ resource "aws_subnet" "private1" {
   }
 }
 
-###################################################################
-# Creating Private Subnet2
-###################################################################
-
-resource "aws_subnet" "private2" {
-    
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr,var.vpc_subnets,4)
-  map_public_ip_on_launch = false
-  availability_zone       = data.aws_availability_zones.az.names[1]
-  tags = {
-    Name = "${var.project}-private2"
-    Project = var.project
-  }
-  lifecycle {
-    create_before_destroy = false
-  }
-}
-
-###################################################################
-# Creating Private Subnet3
-###################################################################
-
-resource "aws_subnet" "private3" {
-    
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr,var.vpc_subnets,5)
-  map_public_ip_on_launch = false
-  availability_zone       = data.aws_availability_zones.az.names[2]
-  tags = {
-    Name = "${var.project}-private3"
-    Project = var.project
-  }
-  lifecycle {
-    create_before_destroy = false
-  }
-}
