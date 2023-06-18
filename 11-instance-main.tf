4aedf424296269460912a9f53c71c9b6b2ad85ac
rajusw804 <46264561+rajusw804@users.noreply.github.com>
2023-06-18 11:32:04 +0530

    Update 11-instance-main.tf

--- 11-instance-main.tf
+++ 11-instance-main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "bastion" {
 
   ami                          =  var.ami
   instance_type                =  var.type
-  subnet_id                    =  aws_subnet.public2.id
+  subnet_id                    =  aws_subnet.public1.id
   vpc_security_group_ids       =  [ aws_security_group.bastion.id]
   key_name                     =  aws_key_pair.key.id
   tags = {
